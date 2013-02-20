9b4cf4c08e5665393dbf035f79ea520bb82afde6
Nicholas Jordon <nicholas.jordon@projectcleverweb.com>
2013-02-20 16:25:12 -0500

    started colors

--- lib/theme/header.php
+++ lib/theme/header.php
@@ -1,4 +1,117 @@
-<h1>It Works!</h1>
+<!DOCTYPE html>
+	<html itemscope itemtype="http://schema.org/Organization" xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Project CleverWeb</title>
+		
+		<!-- Disable compatibility mode option in Internet Explorer.
+		Make sure this is the first meta tag in the head element. -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="description" content="If you can make Facebook page, You could use Project CleverWeb to make your own site." />
+		<meta 
+			name="keywords"
+			content="Blog, Profile, Portfolio, Generator, Clever, CleverWeb, Project, Project CleverWeb, Logic Hierarchy, HTML5, Integrated, Developer, Smart, Web, CMS" />
+		<meta name="revisit-after" content="1 days" />
+		<meta name="title" content="Project CleverWeb ~ Free Done Right" />
+		<meta name="generator" content="Project CleverWeb v0.1 pre-alpha" />
+		<meta name="author" content="Nicholas Jordon" />
+		<meta name="contact" content="Webmaster@ProjectCleverWeb.com" />
+		<meta name="language" content="en" />
+		<meta name="subject" content="Project CleverWeb" />
+		<meta http-equiv="last-modified" content="YYYY-MM-DD@hh:mm:ss TMZ" />
+		<meta name="robots" content="All, NOYDIR, NOODP" />
+		<meta name="copyright" content="Nicholas Jordon" />
+		<meta name="googlebot" content="noarchive" />
+		<meta name="no-email-collection" content="All email collection is forbidden!" />
+		<meta http-equiv="Cache-control" content="private" />
+		<meta name="MSSmartTagsPreventParsing" content="true" />
+		
+		<!-- Social Meta-Tags -->
+		<meta itemprop="name" content="Project CleverWeb" />
+		<link rel="publisher" href="https://plus.google.com/u/0/102011089322918303020" />
+		<meta itemprop="description" content="If you can make Facebook page, You could use Project CleverWeb to make your own site." />
+		<meta itemprop="image" content="http://projectcleverweb.com/logos/white-bg/icon/200x200.jpg" />
+		<meta property="og:image" content="http://projectcleverweb.com/css/images/featured/main.png" /> 
+		<meta property="og:title" content="Project CleverWeb ~ Free Done Right" /> 
+		<meta property="og:url" content="http://ProjectCleverWeb.com/" />
+		<meta property="og:site_name" content="Project CleverWeb ~ Free Done Right" />
+		<meta property="fb:admins" content="100003765475496" />
+		<meta property="og:description" content="Discover how using Project CleverWeb makes the difference, by making everything simply work. If you can make Facebook page, You could use Project CleverWeb." /> 
+		<!-- Social Links -->
+		<link rel="icon" type="image/png" href="./logos/transparent/icon/50x50.png" />
+		<link rel="canonical" href="http://ProjectCleverWeb.com/" />
+		<link rel="image_src" href="http://projectcleverweb.com/css/images/featured/main.png" />
+		
+		
+		<!-- Stylesheets -->
+		<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/reset.css" />
+		<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/main.css" />
+		<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/colors.css" />
+		
+		<!-- Internet Explorer -->
+		<!--[if IE]><link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/ie.css" /><![endif]-->
+		<!--[if lt IE 9]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<!--[if gte IE 9]>
+			<style type="text/css">
+				.gradient {
+					 filter: none;
+				}
+			</style>
+		<![endif]-->
+		
+		<!-- JS -->
+		<script src='http://code.jquery.com/jquery-1.7.2.min.js' type='text/javascript'></script>
+		<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+	</head>
+	<body class"gradient">
+		<div id="page-container">
+			<header class="header" role="banner">
+				<table>
+					<tr>
+						<td style="text-align: left;">
+							<p id="logo" class="h1"><a href="#" rel="nofollow"><h1>It Works!</h1></a></p>
+						</td>
+						<td style="text-align: right;">
+							
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<nav role="navigation">
+								
+								
+								
+							</nav>
+						</td>
+					</tr>
+				</table>
+			</header>
+			
+			<span class="test gradient_red">
+				Bla bla bla bla bla bla
+			</span>
+			
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			
+			
+			
+		</div>
+		<footer class="footer" role="contentinfo">
+			<div id="inner-footer" class="wrap clearfix">
+				<nav role="navigation">
+				</nav>
+				<p class="source-org copyright">&copy; Project CleverWeb.</p>
+			</div>
+		</footer>
+		<span class="hidden" id="eof-scripts">
+			<!-- A Place for all the eof js -->
+			
+		</span>
+	</body>
+</html>
 
 <?php
 // The header of the page
