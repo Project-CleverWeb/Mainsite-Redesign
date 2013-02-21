1baedf2985a25a3a2708fa1982eb7bd12ab925c0
Nicholas Jordon <nicholas.jordon@projectcleverweb.com>
2013-02-21 16:48:44 -0500

    made progress on page-to-page design. IE <=8 is very plain.

--- lib/theme/header.php
+++ lib/theme/header.php
@@ -61,6 +61,9 @@
 				}
 			</style>
 		<![endif]-->
+		<!--[if lte IE 9]>
+		<script src="./lib/theme/lib/js/curvycorners/jquery.curvycorners.min.js"></script>
+		<![endif]-->
 		
 		<!-- JS -->
 		<script src='http://code.jquery.com/jquery-1.7.2.min.js' type='text/javascript'></script>
@@ -72,7 +75,7 @@
 				<table>
 					<tr>
 						<td style="text-align: left;">
-							<p id="logo" class="h1"><a href="#" rel="nofollow"><h1>It Works!</h1></a></p>
+							<img id="toplogoimg" src="http://projectcleverweb.com/css/images/logos/CW_Logo_Sm_NT.png" height="79px" alt="Project CleverWeb Logo" />
 						</td>
 						<td style="text-align: right;">
 							
@@ -90,11 +93,119 @@
 				</table>
 			</header>
 			
-			<span class="test button gradient_diag_red">
-				Bla bla bla bla bla bla
-			</span>
+			<h1>The Header <a href="#">#</a> 1</h1>
+			<h2>The Header <a href="#">#</a> 2</h2>
+			<h3>The Header <a href="#">#</a> 3</h3>
+			<h4>The Header <a href="#">#</a> 4</h4>
+			<h5>The Header <a href="#">#</a> 5</h5>
+			<h6>The Header <a href="#">#</a> 6</h6>
 			
-			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam diam in urna aliquet tincidunt. Nam euismod placerat mi. In hac habitasse platea dictumst. Phasellus eu semper enim. Nam ante quam, vulputate eget dictum sed, ullamcorper sed quam. Nullam eu quam risus, auctor facilisis lectus. Cras sagittis mauris id turpis suscipit at luctus sem gravida. Vestibulum euismod volutpat enim. Vestibulum magna erat, <a href="#">This Link bla bla bla</a> dignissim quis varius in, vulputate eu dolor. Sed sit amet nisl urna. Nam pulvinar ligula eu sem lacinia sit amet scelerisque enim porta. Ut vulputate vulputate augue, at vulputate dui vulputate a. Nullam dictum, arcu in accumsan fringilla, justo massa tincidunt sapien, eget mattis nibh ligula vel eros. Vivamus interdum vestibulum convallis. Aliquam ut tortor in tortor ultricies rhoncus.</p>
+
+<p><b>Integer mattis lacinia quam dapibus suscipit. Praesent facilisis erat a nunc volutpat non condimentum nisi volutpat. Aliquam eu bibendum dolor. Fusce tincidunt urna at turpis consectetur nec tempor purus porta. Morbi vel lectus vel lectus eleifend semper ut id urna. Proin quis dignissim purus. <a href="#">This Link bla bla bla</a> Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer condimentum porta purus non mollis. Nam est purus, commodo semper faucibus nec, consectetur in tellus. Nunc pellentesque ullamcorper lectus sed condimentum. Sed at commodo lectus. Etiam quam nibh, fermentum quis consectetur vitae, molestie eu est. Vestibulum sit amet velit quis neque egestas porta. Mauris adipiscing tristique nibh, nec tincidunt lorem tempor vitae. In fringilla sollicitudin urna vulputate malesuada. Vestibulum malesuada ultrices lobortis.</b></p>
+
+<p><i>Duis ac diam metus, nec bibendum orci. Duis ullamcorper placerat erat, quis varius odio convallis vitae. Donec adipiscing commodo libero at lacinia. Aenean convallis nulla eu arcu cursus nec posuere urna pretium. Cras a iaculis ipsum. Cras non metus a libero consectetur fringilla et eu elit. <a href="#">This Link bla bla bla</a> Donec facilisis interdum lectus, quis gravida augue posuere nec. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam ut ante vel mauris pellentesque egestas in feugiat libero. Vivamus mattis, mi sit amet consectetur volutpat, diam enim aliquam orci, et pellentesque sapien sapien eget nisl. Nulla sit amet odio magna, nec malesuada leo. Mauris tellus sem, hendrerit id mattis nec, scelerisque ut felis. Integer consectetur, arcu quis porta cursus, lectus metus hendrerit purus, nec tincidunt nisi mi ac justo.</i></p>
+
+<p><u>Vestibulum laoreet urna et orci facilisis mollis. Etiam in augue ac tortor cursus bibendum. Suspendisse augue nibh, facilisis in tempus at, condimentum eget ipsum. Sed vehicula egestas rhoncus. Maecenas dapibus libero at elit semper sit amet cursus dolor gravida. Nullam vitae nulla ac massa laoreet vulputate. In sagittis, purus quis vulputate porttitor, ligula augue ultricies urna, in faucibus lectus turpis id metus. <a href="#">This Link bla bla bla</a> Proin mi leo, euismod sed gravida sed, tempus egestas augue. Maecenas leo mi, faucibus a sagittis vel, sodales sed eros.</u></p>
+
+<p><b><i>Maecenas a sem massa, vitae suscipit lacus. Nullam nec lorem sed purus consectetur condimentum. Phasellus ornare consectetur sapien. Donec suscipit turpis at lorem viverra bibendum. Sed accumsan lacus eget nibh vulputate fringilla. Nunc ac vestibulum ante. Praesent volutpat sagittis justo ac porta. Suspendisse potenti. Aenean aliquam, ligula a volutpat ultricies, lorem sapien facilisis dolor, <a href="#">This Link bla bla bla</a> eget condimentum ipsum nibh sed dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus orci, luctus sed rhoncus eu, placerat vel lacus. Maecenas tempor lacinia ultricies. Aliquam ultrices, lectus ut accumsan imperdiet, odio nisl tempor tortor, eget pulvinar sapien est eget est.</i></b></p>
+			
+			
+			<pre>
+				&lt;?
+$path = &quot;/YOUR/PATH/TO/public_html/php_diary/data&quot;;
+$filename = &quot;122499.dat&quot;;
+$x= -1;
+  if($file = fopen(&quot;$path/$filename&quot;, &quot;r&quot;))
+  {
+    while(!feof($file))
+    {
+      $therate = fgetss($file, 255);
+      $x++;
+      $count = $count + $therate;
+    }
+    fclose($file);
+  }
+$average = ($count / $x);
+print(&quot;Surfer Average Rating for 12/24/99: &quot;);
+printf(&quot;%.2f&quot;, $average);
+print(&quot;&lt;br&gt;Total Surfer Votes: $x&quot;);
+?&gt;
+			</pre>
+			
+			<code>
+				&lt;?
+$path = &quot;/YOUR/PATH/TO/public_html/php_diary/data&quot;;
+$filename = &quot;122499.dat&quot;;
+$x= -1;
+  if($file = fopen(&quot;$path/$filename&quot;, &quot;r&quot;))
+  {
+    while(!feof($file))
+    {
+      $therate = fgetss($file, 255);
+      $x++;
+      $count = $count + $therate;
+    }
+    fclose($file);
+  }
+$average = ($count / $x);
+print(&quot;Surfer Average Rating for 12/24/99: &quot;);
+printf(&quot;%.2f&quot;, $average);
+print(&quot;&lt;br&gt;Total Surfer Votes: $x&quot;);
+?&gt;
+			</code>
+			
+			<pre>
+				<code>
+					&lt;?
+$path = &quot;/YOUR/PATH/TO/public_html/php_diary/data&quot;;
+$filename = &quot;122499.dat&quot;;
+$x= -1;
+  if($file = fopen(&quot;$path/$filename&quot;, &quot;r&quot;))
+  {
+    while(!feof($file))
+    {
+      $therate = fgetss($file, 255);
+      $x++;
+      $count = $count + $therate;
+    }
+    fclose($file);
+  }
+$average = ($count / $x);
+print(&quot;Surfer Average Rating for 12/24/99: &quot;);
+printf(&quot;%.2f&quot;, $average);
+print(&quot;&lt;br&gt;Total Surfer Votes: $x&quot;);
+?&gt;
+				</code>
+			</pre>
+			
+			
+			<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+				class="tridem radcorn flip gradient_white gradient">
+					Button</span>
+			<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+				class="tridem radcorn flip gradient_horz_white gradient">
+					Button</span>
+			<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+				class="tridem radcorn flip gradient_diag_white gradient">
+					Button</span>
+			<br />
+			<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+				class="tridem radcorn flip gradient_split_white gradient">
+					Button</span>
+			<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+				class="tridem radcorn flip gradient_split_horz_white gradient">
+					Button</span>
+			<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+				class="tridem radcorn flip gradient_split_diag_white gradient">
+					Button</span>
+			
+			
+			
+			
+			
+			
 			
 			
 			
