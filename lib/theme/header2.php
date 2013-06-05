079672e4840c0c5b5cddb80ea7a4af204d31ec07
Nicholas J <administrator@projectcleverweb.com>
2013-06-05 15:26:13 -0400

    initial commit for working changes

---
+++ lib/theme/header2.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" itemtype="http://schema.org/Organization" xmlns="http://www.w3.org/1999/xhtml"> <!--<![endif]-->
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Project CleverWeb</title>
+		
+		<!-- Misc Meta -->
+		<meta name="viewport" content="width=device-width">
+		<meta name="description" content="If you can make Facebook page, You could use Project CleverWeb to make your own site." />
+		<meta 
+			name="keywords"
+			content="Blog, Profile, Portfolio, Generator, Clever, CleverWeb, Project, Project CleverWeb, Logic Hierarchy, HTML5, Integrated, Developer, Smart, Web, CMS" />
+		<meta name="revisit-after" content="1 days" />
+		<meta name="title" content="Project CleverWeb ~ Free Done Right" />
+		<meta name="generator" content="Project CleverWeb v0.1 pre-alpha" />
+		<meta name="author" content="Nicholas Jordon" />
+		<meta name="contact" content="Webmaster@ProjectCleverWeb.com" />
+		<meta name="language" content="en" />
+		<meta name="subject" content="Project CleverWeb" />
+		<meta http-equiv="last-modified" content="YYYY-MM-DD@hh:mm:ss TMZ" />
+		<meta name="robots" content="All, NOYDIR, NOODP" />
+		<meta name="copyright" content="Nicholas Jordon" />
+		<meta name="googlebot" content="noarchive" />
+		<meta name="no-email-collection" content="All email collection is forbidden!" />
+		<meta http-equiv="Cache-control" content="private" />
+		<meta name="MSSmartTagsPreventParsing" content="true" />
+		
+		<!-- Social Metas-->
+		<meta itemprop="name" content="Project CleverWeb" />
+		<meta itemprop="description" content="If you can make Facebook page, You could use Project CleverWeb to make your own site." />
+		<meta itemprop="image" content="http://projectcleverweb.com/logos/white-bg/icon/200x200.jpg" />
+		<meta property="og:image" content="http://projectcleverweb.com/css/images/featured/main.png" /> 
+		<meta property="og:title" content="Project CleverWeb ~ Free Done Right" /> 
+		<meta property="og:url" content="http://ProjectCleverWeb.com/" />
+		<meta property="og:site_name" content="Project CleverWeb ~ Free Done Right" />
+		<meta property="fb:admins" content="100003765475496" />
+		<meta property="og:description" content="Discover how using Project CleverWeb makes the difference, by making everything simply work. If you can make Facebook page, You could use Project CleverWeb." />
+		
+		<!-- Social Links -->
+		<link rel="publisher" href="https://plus.google.com/u/0/102011089322918303020" />
+		<link rel="icon" type="image/png" href="./logos/transparent/icon/50x50.png" />
+		<link rel="canonical" href="http://ProjectCleverWeb.com/" />
+		<link rel="image_src" href="http://projectcleverweb.com/css/images/featured/main.png" />
+		
+		<!-- Stylesheets -->
+		<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/bootstrap-responsive.css" />
+		<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/simple-css3.css" />
+		<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/highlight.js.github.css">
+		<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/boilerplate.css" />
+		<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/main.css" />
+		
+		
+		<!-- Internet Explorer w/o Google Chrom Frame -->
+		<!-- get as much support as possible, will still look off in areas for 8 or less -->
+		<!--[if IE]>
+			<link rel="stylesheet" type="text/css" href="./lib/theme/lib/css/ie.css" />
+		<![endif]-->
+		<!--[if lt IE 9]>
+			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<!--[if gte IE 9]>
+			<style type="text/css">
+				.gradient {
+					filter: none;
+				}
+			</style>
+		<![endif]-->
+		<!--[if lte IE 9]>
+			<style type="text/css">
+				.gradient {
+					behavior: url(./lib/theme/lib/other/PIE.php);
+				}
+			</style>
+		<![endif]-->
+		
+		<!-- JS -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" src="//apis.google.com/js/plusone.js"></script>
+		<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<!-- Older versions of IE dont like the highlighter so leave it out then -->
+		<![if !IE]>
+			<script type="text/javascript" src="./lib/theme/lib/js/highlight.js/highlight.pack.js"></script>
+			<script>hljs.initHighlightingOnLoad();</script>
+		<![endif]>
+	</head>
+	<body>
+		
+		<div id="page-wrapper">
+			<div id="page-container">
+				
+				
+				<div class="header-container">
+					<header class="wrapper clearfix header" role="banner">
+						<h1 class="title"></h1>
+							<object data="./lib/theme/lib/images/logo.svg" height="79px" type="image/svg+xml">
+								<img src="http://projectcleverweb.com/css/images/logos/CW_Logo_Sm_NT.png" />
+							</object>
+						<nav>
+							<ul>
+								<li><a class="here" href="#">Home</a></li>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Contact</a></li>
+								<li><a href="#">Support Us</a></li>
+							</ul>
+						</nav>
+					</header>
+				</div>
+				
+				<!--[if lt IE 8]>
+					<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+				<![endif]-->
+				
+				<div class="main-container">
+					<div class="main wrapper clearfix">
+						
+						<article>
+							<header>
+								<span class="test-title">### HEADERS ###</span><br />
+								<h1>The Header <a href="#">#</a> 1</h1>
+								<h2>The Header <a href="#">#</a> 2</h2>
+								<h3>The Header <a href="#">#</a> 3</h3>
+								<h4>The Header <a href="#">#</a> 4</h4>
+								<h5>The Header <a href="#">#</a> 5</h5>
+								<h6>The Header <a href="#">#</a> 6</h6>
+								
+								<span class="test-title">### TEXT ###</span><br />
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed harum reprehenderit nemo doloribus earum dolor rerum adipisci optio voluptatibus quaerat quasi odit possimus asperiores atque sint maxime excepturi facilis soluta ex ea saepe aliquam provident incidunt doloremque unde quia similique!</p>
+								
+								<p>
+									<i>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed harum reprehenderit nemo doloribus earum dolor rerum adipisci optio voluptatibus quaerat quasi odit possimus asperiores atque sint maxime excepturi facilis soluta ex ea saepe aliquam provident incidunt doloremque unde quia similique!</i>
+								</p>
+								
+								<p>
+									<b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed harum reprehenderit nemo doloribus earum dolor rerum adipisci optio voluptatibus quaerat quasi odit possimus asperiores atque sint maxime excepturi facilis soluta ex ea saepe aliquam provident incidunt doloremque unde quia similique!</b>
+								</p>
+								
+								<p>
+									<i><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed harum reprehenderit nemo doloribus earum dolor rerum adipisci optio voluptatibus quaerat quasi odit possimus asperiores atque sint maxime excepturi facilis soluta ex ea saepe aliquam provident incidunt doloremque unde quia similique!</b></i>
+								</p>
+								
+								
+								<span class="test-title">### CODE ###</span><br />
+								<pre><code>&lt;?php
+$path = &quot;/YOUR/PATH/TO/public_html/php_diary/data&quot;;
+$filename = &quot;122499.dat&quot;;
+$x= -1;
+	if($file = fopen(&quot;$path/$filename&quot;, &quot;r&quot;))
+	{
+		while(!feof($file))
+		{
+			$therate = fgetss($file, 255);
+			$x++;
+			$count = $count + $therate;
+		}
+		fclose($file);
+	}
+$average = ($count / $x);
+print(&quot;Surfer Average Rating for 12/24/99: &quot;);
+printf(&quot;%.2f&quot;, $average);
+print(&quot;&lt;br&gt;Total Surfer Votes: $x&quot;);
+?&gt;</code></pre>
+							</header>
+							<section>
+								<span class="test-title">### FORM ###</span><br />
+								<form action="" method="post">
+									Name: <input type="text" name="name" placeholder="John Doe" value="" maxlength="50" /><br />
+									Email: <input type="text" name="email" value="" placeholder="me@example.com" maxlength="50" /><br />
+									Message: 
+									<textarea name="msg"></textarea>
+									<br />
+									<input type="radio" name="rdChoice" value="Mag3" checked/>
+										Magazine 3
+									<input type="radio" name="rdChoice" value="Mag4" />
+										Magazine 4
+									<br />
+									<input type="checkbox" name="chk" value="" checked/> Check Me!<br />
+									<div class="align_r"><input type="submit" value="Submit This Form"></div>
+								</form>
+							<footer>
+								<!--
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_grey gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_horz_grey gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_grey gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_grey gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_horz_grey gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_grey gradient">
+										Button</span>
+								<br />
+								<br />
+								
+								
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_red gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_red gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_blue gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_blue gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_yellow gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_yellow gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_purple gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_purple gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_orange gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_orange gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_green gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_green gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_pink gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_pink gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_brown gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_brown gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_black gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_black gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_white gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_white gradient">
+										Button</span>
+								<br />
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_diag_grey gradient">
+										Button</span>
+								<span style="padding:50px 100px 50px 100px; line-height: 130px;margin: 5px;"
+									class="tridem_light radcorn flip gradient_split_diag_grey gradient">
+										Button</span> -->
+							</footer>
+						</article>
+
+						<aside>
+							<span class="test-title">### ASIDE ###</span><br />
+							<p>
+								<table>
+									<tr>
+										<td>
+											Efficientcy meets dynamic design with HTML5
+										</td>
+										<td>
+											<a href="http://www.w3.org/html/logo/">
+												<img src="http://www.w3.org/html/logo/badge/html5-badge-v-css3-device-graphics-multimedia-performance-semantics.png" width="38" height="266" alt="HTML5 Powered with CSS3 / Styling, Device Access, Graphics, 3D &amp; Effects, Multimedia, Performance &amp; Integration, and Semantics" title="HTML5 Powered with CSS3 / Styling, Device Access, Graphics, 3D &amp; Effects, Multimedia, Performance &amp; Integration, and Semantics">
+											</a>
+										</td>
+									</tr>
+								</table>
+							</p>
+						</aside>
+						<aside>
+							<span class="test-title">### ASIDE ###</span><br />
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+						</aside>
+
+					</div> <!-- #main -->
+				</div> <!-- #main-container -->
+
+				<div class="footer-container">
+					<footer class="wrapper footer" role="contentinfo">
+						<div id="inner-footer" class="wrap clearfix">
+							<nav role="navigation">
+							</nav>
+							<p class="source-org copyright">&copy; Project CleverWeb.</p>
+						</div>
+					</footer>
+				</div>
+				
+				
+				<span class="hidden" id="eof-scripts">
+					<!-- A Place for all the eof js -->
+					<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+					<script src="js/plugins.js"></script>
+					<script src="js/main.js"></script>
+					
+					<script>
+						var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+						(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+						g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+						s.parentNode.insertBefore(g,s)}(document,'script'));
+					</script>
+				</span>
+				
+			</div>
+		</div>
+		
+	</body>
+</html><?php 
+
+
+
+
